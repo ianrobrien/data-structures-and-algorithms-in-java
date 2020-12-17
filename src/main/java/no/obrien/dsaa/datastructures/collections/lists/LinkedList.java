4fbee3470d0eb70cb4b6b84e9b98f7237a22769a
Ian Robert O'Brien <Ian.Robert.OBrien@dnb.no>
2020-12-17 10:15:21 +0100

    Implemented additional LinkedList methods

--- src/main/java/no/obrien/dsaa/datastructures/collections/lists/LinkedList.java
+++ src/main/java/no/obrien/dsaa/datastructures/collections/lists/LinkedList.java
@@ -1,8 +1,6 @@
 package no.obrien.dsaa.datastructures.collections.lists;
 
-import no.obrien.dsaa.datastructures.collections.Collection;
-
-public class LinkedList<T> implements Collection<T> {
+public class LinkedList<T> implements List<T> {
 
   private int size;
   private ListNode<T> head;
@@ -19,11 +17,32 @@ public class LinkedList<T> implements Collection<T> {
       tail.setNext(new ListNode<>(item));
     }
     this.size++;
-
   }
 
   @Override
   public void remove(T item) {
+    if (this.head != null) {
+      if (this.head.getValue() == item) {
+        ListNode<T> temp = this.head;
+        this.head = this.head.getNext();
+        temp.setNext(null); // prevent memory leak by removing reference to next node
+        this.size--;
+      } else {
+        ListNode<T> previous = this.head;
+        ListNode<T> current = this.head.getNext();
+        while (current != null) {
+          if (current.getValue() == item) {
+            previous.setNext(current.getNext());
+            current.setNext(null); // prevent memory leak by removing reference to next node
+            this.size--;
+            return;
+          } else {
+            previous = current;
+            current = current.getNext();
+          }
+        }
+      }
+    }
   }
 
   @Override
@@ -33,6 +52,18 @@ public class LinkedList<T> implements Collection<T> {
 
   @Override
   public void clear() {
+    if (this.head != null) {
+      ListNode<T> previous = head;
+      ListNode<T> current = head.getNext();
+      while (current != null) {
+        previous.setNext(null);
+        previous = current;
+        current = current.getNext();
+        this.size--;
+      }
+      this.head = null;
+      this.size--;
+    }
   }
 
   @Override
@@ -46,6 +77,18 @@ public class LinkedList<T> implements Collection<T> {
     }
 
     return false;
+  }
+
+  @Override
+  public T get(int index) {
+    if (index < 0 || index > this.size - 1) {
+      throw new IndexOutOfBoundsException();
+    }
+    ListNode<T> current = this.head;
+    for (int i = 0; i < index; i++) {
+      current = current.getNext();
+    }
+    return current.getValue();
   }
 
   static class ListNode<T> {
--- src/test/java/no/obrien/dsaa/datastructures/lists/LinkedListTest.java
+++ src/test/java/no/obrien/dsaa/datastructures/lists/LinkedListTest.java
@@ -2,6 +2,7 @@ package no.obrien.dsaa.datastructures.lists;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import no.obrien.dsaa.datastructures.collections.lists.LinkedList;
@@ -30,5 +31,59 @@ class LinkedListTest {
     assertTrue(linkedList.contains(1));
     assertTrue(linkedList.contains(2));
     assertFalse(linkedList.contains(9));
+  }
+
+  @Test
+  void testRemove() {
+    LinkedList<Integer> linkedList = new LinkedList<>();
+    linkedList.add(0);
+    linkedList.add(1);
+    linkedList.add(2);
+    linkedList.add(3);
+    linkedList.add(4);
+
+
+    linkedList.remove(0); // remove from head
+    assertEquals(4, linkedList.size());
+
+    linkedList.remove(2); // remove from middle
+    assertEquals(3, linkedList.size());
+
+    linkedList.remove(4); // remove from tail
+    assertEquals(2, linkedList.size());
+
+    assertFalse(linkedList.contains(0));
+    assertFalse(linkedList.contains(2));
+    assertFalse(linkedList.contains(4));
+
+    assertTrue(linkedList.contains(1));
+    assertTrue(linkedList.contains(3));
+  }
+
+  @Test
+  void testGet() {
+    LinkedList<Integer> linkedList = new LinkedList<>();
+    linkedList.add(0);
+    linkedList.add(1);
+    linkedList.add(2);
+    linkedList.add(3);
+    linkedList.add(4);
+
+    assertThrows(IndexOutOfBoundsException.class, () -> linkedList.get(-1));
+    assertThrows(IndexOutOfBoundsException.class, () -> linkedList.get(10));
+    assertEquals(3, linkedList.get(3));
+  }
+
+  @Test
+  void testClear() {
+    LinkedList<Integer> linkedList = new LinkedList<>();
+    linkedList.add(0);
+    linkedList.add(1);
+    linkedList.add(2);
+    linkedList.add(3);
+    linkedList.add(4);
+
+    linkedList.clear();
+    assertEquals(0, linkedList.size());
   }
 }
