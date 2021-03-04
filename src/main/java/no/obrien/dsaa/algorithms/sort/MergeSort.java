349f4f41bbbfd79f8b8f238531e5cd903c6ce839
Ian Robert O'Brien <ian.r.obrien@protonmail.com>
2021-03-04 11:30:03 +0100

    Added merge sort initial implementation

--- src/main/java/no/obrien/dsaa/algorithms/sort/MergeSort.java
+++ src/main/java/no/obrien/dsaa/algorithms/sort/MergeSort.java
@@ -4,22 +4,50 @@ public class MergeSort<T extends Comparable<T>> implements Sorter<T> {
 
   @Override
   public T[] sort(T[] elements) {
-    sort(elements, 0, elements.length - 1);
+    sort(elements, elements.length);
     return elements;
   }
 
-  void merge(T[] elements, int left, int middle, int right) {
-    int sizeFirst = middle - left + 1;
-    int sizeSecond = right - middle;
+  void merge(T[] elements, T[] left, T[] right, int beginning, int end) {
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    while (i < beginning && j < end) {
+      if (left[i].compareTo(right[j]) <= 0) {
+        elements[k++] = elements[i++];
+      } else {
+        elements[k++] = right[j++];
+      }
+    }
+    while (i < beginning) {
+      elements[k++] = left[i++];
+    }
+    while (j < end) {
+      elements[k++] = right[j++];
+    }
   }
 
-  void sort(T[] elements, int left, int right) {
-    if (left < right) {
-      int middle = 1 + (right - 1) / 2;
-      sort(elements, left, middle);
-      sort(elements, middle + 1, right);
-
-      merge(elements, left, middle, right);
+  void sort(T[] elements, int end) {
+    if (end < 2) {
+      return;
     }
+    int middle = end / 2;
+
+    Comparable<T>[] left = new Comparable[middle];
+    Comparable<T>[] right = new Comparable[end - middle];
+
+    for (int i = 0; i < middle; i++) {
+      left[i] = elements[i];
+    }
+
+    for (int i = middle; i < end; i++) {
+      right[i - middle] = elements[i];
+    }
+
+    sort((T[]) left, middle);
+    sort((T[]) right, end - middle);
+
+    merge(elements, (T[]) left, (T[]) right, middle, end - middle);
   }
 }
